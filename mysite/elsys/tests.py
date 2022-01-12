b972f3bf6df58cece1fe10e8cf4c1893c9152526
BoyanPavlov03 <boyan.p.pavlov@gmail.com>
2022-01-12 21:10:22 +0200

    Fixed something little

--- mysite/elsys/tests.py
+++ mysite/elsys/tests.py
@@ -13,16 +13,16 @@ class TestApiTemperatureModel(TestCase):
         pass
 
     @patch("requests.post")
-    def test_minimum_temperature(self):
+    def test_minimum_temperature(self, mocked_requests):
         mocked_requests.return_value.json = Mock(return_value = self.data)
         assert ApiTemperature.min_temp() == 3
 
     @patch("requests.post")
-    def test_maximum_temperature(self):
+    def test_maximum_temperature(self, mocked_requests):
         mocked_requests.return_value.json = Mock(return_value = self.data)
         assert ApiTemperature.max_temp() == 8
 
     @patch("requests.post")
-    def test_average_temperature(self):
+    def test_average_temperature(self, mocked_requests):
         mocked_requests.return_value.json = Mock(return_value = self.data)
         assert ApiTemperature.avg_temp() == 5.0
